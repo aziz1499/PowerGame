d89b571c4861aea325dfaaeb4d9f6aa0dca2bcfe
aziz1499 <cherifaziz601@gmail.com>
2025-04-08 06:58:05 +0100

    structure de donnees (ajout d'un fichier)
    Une List<GameObject> appelée collectibles (liste d’objets ramassables).

---
+++ PowerGame/Assets/Scripts/CollectibleManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleManager : MonoBehaviour
+{
+    [Header("Liste des objets ramassables")]
+    public List<GameObject> collectibles = new List<GameObject>();
+
+    void Update()
+    {
+        // Appuyer sur C pour afficher les objets dans la console
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ShowCollectibles();
+        }
+    }
+
+    void ShowCollectibles()
+    {
+        if (collectibles.Count == 0)
+        {
+            Debug.Log("Aucun objet ramassable n'est défini.");
+            return;
+        }
+
+        Debug.Log("Objets ramassables :");
+        foreach (GameObject obj in collectibles)//Une boucle foreach pour afficher les objets de la liste dans la console.
+        {
+            Debug.Log($"- {obj.name}");
+        }
+    }
+}
